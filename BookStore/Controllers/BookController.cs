68b82d92bbdc7459a53b3982afed158950f896cd
mcanbildiren <78155451+mcanbildiren@users.noreply.github.com>
2022-05-15 00:22:28 +0300

    crud basic

--- BookStore/Controllers/BookController.cs
+++ BookStore/Controllers/BookController.cs
@@ -38,6 +38,7 @@ namespace BookStore.Controllers
         public List<Book> GetBooks()
         {
             var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+
             return bookList;
         }
 
@@ -45,6 +46,7 @@ namespace BookStore.Controllers
         public Book GetById(int id)
         {
             var book = BookList.Where(x => x.Id == id).SingleOrDefault();
+
             return book;
         }
 
@@ -54,5 +56,46 @@ namespace BookStore.Controllers
         //    var book = BookList.Where(x => x.Id == Convert.ToInt32(id)).SingleOrDefault();
         //    return book;
         //}
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book newBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+
+            if (book is not null)
+                return BadRequest();
+
+            BookList.Add(newBook);
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+
+            if (book is null)
+                return BadRequest();
+
+            book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+            if (book is null)
+                return BadRequest();
+
+            BookList.Remove(book);
+
+            return Ok();
+        }
     }
 }
